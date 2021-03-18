d9238815d638d691966e5bb3b809db7844bfc86e
Sean Leif Hellier <sean@newtropolis.com>
2021-03-18 18:11:24 -0400

    building tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+      <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '../src/components/EmployeeTable'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    EmployeeTable,
+  },
+   data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
   }
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
